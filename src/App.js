57969d5077364b146d72cc351fb3e0c3cc2f463f
S@TwIt <satwikmohantyskm@gmail.com>
2022-08-24 22:00:01 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <h1>Om Namo Narayana</h1>
+      <Home />
+
     </div>
   );
 }
